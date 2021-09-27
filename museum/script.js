78eeb110b1776f918a944040cf122abf7884791a
akmarzhan1 <akmarzhan@minerva.kgi.edu>
2021-09-28 01:54:23 +0200

    init museum

---
+++ museum/script.js
@@ -0,0 +1,55 @@
+$(function(){
+    $('#discover-lo').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785203861!6m8!1m7!1sCAoSLEFGMVFpcE9GRUhBdHFabWo5XzF1ZFA2WGh6Y3VSWW1TVEVqZnlkS3dVYzV6!2m2!1d48.8649723!2d2.3378061!3f0!4f20!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#royal-p').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785203861!6m8!1m7!1sCAoSLEFGMVFpcE9GRUhBdHFabWo5XzF1ZFA2WGh6Y3VSWW1TVEVqZnlkS3dVYzV6!2m2!1d48.8649723!2d2.3378061!3f0!4f20!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#denon-w').click(function(){ 
+        if(!$('#iframe').length) {
+            $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785391109!6m8!1m7!1sCAoSLEFGMVFpcE8zbXJmVkZuUmVFWXdDTWNLTHczZDFPSmw0Y0N2cmdjSHpMTHdn!2m2!1d48.8604546!2d2.3386367!3f282.90997!4f19.919619999999995!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#colon').click(function(){ 
+        if(!$('#iframe').length) {
+                $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785391109!6m8!1m7!1sCAoSLEFGMVFpcE8zbXJmVkZuUmVFWXdDTWNLTHczZDFPSmw0Y0N2cmdjSHpMTHdn!2m2!1d48.8604546!2d2.3386367!3f282.90997!4f19.919619999999995!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#greek-h').click(function(){ 
+        if(!$('#iframe').length) {
+            $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785203861!6m8!1m7!1sCAoSLEFGMVFpcE9GRUhBdHFabWo5XzF1ZFA2WGh6Y3VSWW1TVEVqZnlkS3dVYzV6!2m2!1d48.8649723!2d2.3378061!3f0!4f20!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#mona-l').click(function(){ 
+        if(!$('#iframe').length) {
+            $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785391109!6m8!1m7!1sCAoSLEFGMVFpcE8zbXJmVkZuUmVFWXdDTWNLTHczZDFPSmw0Y0N2cmdjSHpMTHdn!2m2!1d48.8604546!2d2.3386367!3f282.90997!4f19.919619999999995!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
+
+$(function(){
+    $('#night-p').click(function(){ 
+        if(!$('#iframe').length) {
+            $('#iframeHolder').html('<iframe width="100%" height="500" src="https://www.google.com/maps/embed?pb=!4v1632785203861!6m8!1m7!1sCAoSLEFGMVFpcE9GRUhBdHFabWo5XzF1ZFA2WGh6Y3VSWW1TVEVqZnlkS3dVYzV6!2m2!1d48.8649723!2d2.3378061!3f0!4f20!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>');
+        }
+    });   
+});
